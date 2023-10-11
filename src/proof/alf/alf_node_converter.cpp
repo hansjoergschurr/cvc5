7969c3ae3900ef80c1c0bce668298ca88aedc0cd
ajreynol <andrew.j.reynolds@gmail.com>
2023-10-11 10:32:34 -0500

    Avoid clashes

--- src/proof/alf/alf_node_converter.cpp
+++ src/proof/alf/alf_node_converter.cpp
@@ -299,8 +299,10 @@ Node AlfNodeConverter::postConvert(Node n)
   {
     // dummy symbol, provide the return type
     Node tnn = typeAsNode(tn);
+    std::stringstream ss;
+    ss << "@fp." << printer::smt2::Smt2Printer::smtKindString(k); 
     return mkInternalApp(
-        printer::smt2::Smt2Printer::smtKindString(k), {tnn}, tn);
+        ss.str(), {tnn}, tn);
   }
   else if (GenericOp::isIndexedOperatorKind(k))
   {
